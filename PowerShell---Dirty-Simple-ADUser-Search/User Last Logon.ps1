dce854abb5e5ad71fc7d756db7fc9f8884803125
rrice2004 <40141955+rrice2004@users.noreply.github.com>
2019-12-12 16:48:56 -0500

    Add files via upload

---
+++ PowerShell---Dirty-Simple-ADUser-Search/User Last Logon.ps1
@@ -0,0 +1,21 @@
+﻿Clear-Host
+$Time = Get-Date -Format "dddd MM/dd/yyyy HH:mm"
+
+while (1 -ne 2){
+
+$User = $Null
+$User = Read-Host "Enter user to search, or EXIT to quit"
+
+if ($User -eq "EXIT") {exit;}
+
+Write-Host ""
+Write-Host $Time
+Get-ADUser -Identity $User -Properties * | Select-Object Name, LastLogOnDate, Enabled, LockedOut, PasswordExpired, BadLogonCount | Format-list
+
+} else{
+
+Clear-Host
+continue
+
+}
+
